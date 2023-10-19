9100306ec447ab7d8795593ae0f81e911c3b7f47
Jack Kelly <jack.kelly@bellroy.com>
2023-10-19 17:44:06 +1000

    Use our standard FOSS flake

--- flake.nix
+++ flake.nix
@@ -2,44 +2,21 @@
   description = "Types and optics for manipulating Amazon Resource Names (ARNs)";
 
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    bellroy-nix-foss = {
+      url = "github:bellroy/bellroy-nix-foss";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import inputs.nixpkgs { inherit system; };
-        makePackage = haskellPackages:
-          haskellPackages.callPackage ./aws-arn.nix { };
-      in
-      rec
+  outputs = inputs: inputs.bellroy-nix-foss.lib.haskellProject {
+    cabalPackages = [
       {
-        packages = {
-          default = makePackage pkgs.haskellPackages;
-          ghc810 = makePackage pkgs.haskell.packages.ghc810;
-          ghc90 = makePackage pkgs.haskell.packages.ghc90;
-          ghc92 = makePackage pkgs.haskell.packages.ghc92;
-          ghc94 = makePackage pkgs.haskell.packages.ghc94;
-          ghc96 = makePackage pkgs.haskell.packages.ghc96;
-        };
-
-        devShells = builtins.mapAttrs
-          (_: v: v.env.overrideAttrs (oldAttrs: {
-            buildInputs = oldAttrs.buildInputs
-              ++ [ pkgs.nixpkgs-fmt ]
-              ++ (with pkgs.haskellPackages; [
-              cabal-install
-              cabal-fmt
-              doctest
-              haskell-ci
-              hlint
-            ]);
-          }))
-          packages;
-      });
+        name = "aws-arn";
+        path = ./aws-arn.nix;
+      }
+    ];
+    supportedCompilers = [ "ghc810" "ghc90" "ghc92" "ghc94" "ghc96" ];
+    defaultCompiler = "ghc92";
+  };
 }
