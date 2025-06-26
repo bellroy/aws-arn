0ab2aa531331b2be4495b3b75bd3d81b06e4e1c6
Michael Webb <michaelwebb76@gmail.com>
2025-06-26 21:29:34 +1000

    Nixpkgs channel update and nix flake update (#40)

--- flake.nix
+++ flake.nix
@@ -6,21 +6,23 @@
       url = "github:bellroy/bellroy-nix-foss";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs.url = "nixpkgs/nixpkgs-25.05-darwin";
   };
 
-  outputs = inputs: inputs.bellroy-nix-foss.lib.haskellProject {
-    src = ./.;
-    supportedCompilers = [
-      "ghc810"
-      "ghc90"
-      "ghc92"
-      "ghc94"
-      "ghc96"
-      "ghc98"
-      "ghc910"
-      "ghc912"
-    ];
-    defaultCompiler = "ghc96";
-  };
+  outputs =
+    inputs:
+    inputs.bellroy-nix-foss.lib.haskellProject {
+      src = ./.;
+      supportedCompilers = [
+        "ghc810"
+        "ghc90"
+        "ghc92"
+        "ghc94"
+        "ghc96"
+        "ghc98"
+        "ghc910"
+        "ghc912"
+      ];
+      defaultCompiler = "ghc96";
+    };
 }
